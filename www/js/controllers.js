06629630895fcd6ace2bd524a07fcffaec8b7920
Will Xavier <germano@Wills-MacBook-Pro.local>
2016-05-07 01:47:36 -0300

    Adicionando factory de circulares e modificações na aba direto

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,18 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, Circulares) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  $scope.circularesDireto =  Circulares.circularDiretoAll(); //Chats.all();
+  $scope.remove = function(circular) {
+    Circulares.remove(circular);
+  };
 
-  $scope.chats = Chats.all();
+})
+
+.controller('ChatsCtrl', function($scope, Circulares) {
+  
+
+  $scope.chats =  Circulares.circularDireto; //Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,126 @@
 angular.module('starter.services', [])
 
+.factory('Circulares', function(){
+
+  //Lista de circulares
+  var circularDireto = [
+    {
+      id: 0,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '06:00'
+    },
+    {
+      id: 1,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '06:10'
+    },
+    {
+      id: 2,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '06:30'
+    },
+    {
+      id: 3,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '06:38'
+    },
+    {
+      id: 4,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '06:45'
+    },
+    {
+      id: 5,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '06:50'
+    },
+    {
+      id: 6,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '06:55'
+    },
+    {
+      id: 7,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '07:10'
+    },
+    {
+      id: 8,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '07:20'
+    },
+    {
+      id: 9,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '07:25'
+    },
+    {
+      id: 10,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '07:30'
+    },
+    {
+      id: 11,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '07:35'
+    },
+    {
+      id: 12,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '07:53'
+    },
+    {
+      id: 13,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '08:10'
+    },
+    {
+      id: 14,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '08:18'
+    },
+    {
+      id: 15,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '08:35'
+    },
+    {
+      id: 16,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '08:51'
+    },
+    {
+      id: 17,
+      //Tipo 1 = dias uteis, tipo 2 = sábado, tipo 3 = Férias
+      tipo:1,
+      horario: '09:00'
+    },
+  ]
+
+  return{
+    circularDiretoAll: function() {
+      return circularDireto;
+    }
+  };
+})
+
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
